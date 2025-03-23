095b3ab770fd38528727379bf4d18846603de448
NikEnglish <plna1867ss@gmail.com>
2025-03-23 23:19:14 +0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBrnEn_0-kgZGps7rF_q-kGRNmlXjLhC9s",
+  authDomain: "electronic-diary-5a429.firebaseapp.com",
+  databaseURL: "https://electronic-diary-5a429-default-rtdb.firebaseio.com",
+  projectId: "electronic-diary-5a429"
+};
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
